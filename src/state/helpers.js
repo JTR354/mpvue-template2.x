428caf027d59932fcd25ca955f095ee269e3cd28
jtr <jtr354@foxmail.com>
2019-10-28 10:33:23 +0800

    optimize vuex module

--- src/state/helpers.js
+++ src/state/helpers.js
@@ -1,25 +1,20 @@
 // import {mapGetters, mapActions} from 'vuex'
-//
+// HELP METHODS
 // export const cartComputed = {
 //   ...mapGetters('cart', ['count'])
 // }
-//
 // export const orderComputed = {
 //   ...mapGetters('order', ['goodsList', 'total', 'deliverAt', 'couponInfo', 'beforeTotal', 'commodityItem'])
 // }
-//
 // export const orderMethods = {
 //   ...mapActions('order', ['setOrderInfo', 'submitOrder', 'saveCoupon', 'setCommodityItem'])
 // }
-//
 // export const cartMethods = {
 //   ...mapActions('cart', ['setCartCount'])
 // }
-//
 // export const jwtComputed = {
 //   ...mapGetters('jwt', ['token', 'userInfo'])
 // }
-//
 // export const jwtMethods = {
 //   ...mapActions('jwt', ['setToken', 'setUserInfo'])
 // }
